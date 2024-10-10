81dc410ac83af6beda6010a6b7ccf277f24ef644
Sherwin <87555304+owenlim225@users.noreply.github.com>
2024-10-10 22:33:38 +0800

    update 3

---
+++ pokemonArray.py
@@ -0,0 +1,15 @@
+class PokemonArray:
+    def __init__(self) -> None:  
+        self.pokemon_list = [
+                    # [Name, Health, Power, Poisons, Potions, isUsed]
+                    ["Pikachu", 50, 80, 0, 0, False],
+                    ["Charmander", 50, 70, 0, 0, False],
+                    ["Bulbasaur", 60, 60, 0, 0, False],
+                    ["Squirtle", 70, 50, 0, 0, False],
+                    ["Jigglypuff", 70, 45, 0, 0, False],
+                    ["Eevee", 95, 40, 0, 0, False],
+                    ["Snorlax", 150, 90, 0, 0, False],
+                    ["Gengar", 85, 75, 0, 0, False],
+                    ["Machamp", 90, 70, 0, 0, False],
+                    ["Mewtwo", 100, 85, 0, 0, False]
+                ]
